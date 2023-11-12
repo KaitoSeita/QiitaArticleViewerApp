ccd68e277a2c939c7c0e8cdc0788367181817055
kaitoseita <leadik2468@gmail.com>
2023-11-12 17:08:45 +0900

    APIError, Article, Tags, UserのModelの追加

---
+++ QiitaArticleViewer/Model/APIError.swift
@@ -0,0 +1,15 @@
+//
+//  APIError.swift
+//  QiitaArticleViewer
+//
+//  Created by kaito-seita on 2023/11/07.
+//
+
+enum APIError: Error {
+    case unowned
+    case networkTimeOut
+    case invalidStatusCode
+    case noData
+    case noResponse
+    case decodingError
+}
---
+++ QiitaArticleViewer/Model/Article.swift
@@ -0,0 +1,32 @@
+//
+//  Article.swift
+//  QiitaArticleViewer
+//
+//  Created by kaito-seita on 2023/11/05.
+//
+
+import SwiftUI
+
+struct Article: Codable {
+    let comments_count: Int?
+    let created_at: String?
+    let likes_count: Int?
+    let tags: [Tags]
+    let title: String
+    let url: String
+    let user: User
+    let page_views_count: Int?
+}
+
+struct ArticleList: Identifiable {
+    let id = UUID()
+    let commentsCount: Int
+    let createdAt: String
+    let likesCount: Int
+    let tags: [Tags]
+    let title: String
+    let url: String
+    let user: User
+    let viewCount: Int
+}
+// title, urlはオプショナルでない
