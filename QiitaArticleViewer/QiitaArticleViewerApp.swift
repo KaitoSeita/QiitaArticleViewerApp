c2bcda3e0b1a91783b6b885d1316cc4f0a284283
kaitoseita <leadik2468@gmail.com>
2023-11-12 13:53:24 +0900

    Initial Commit

---
+++ QiitaArticleViewer/QiitaArticleViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaArticleViewerApp.swift
+//  QiitaArticleViewer
+//
+//  Created by セイタカイト on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct QiitaArticleViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
