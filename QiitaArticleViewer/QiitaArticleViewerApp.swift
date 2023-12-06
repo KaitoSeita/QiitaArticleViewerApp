d86e61968d107fe229130ad920c9703e1e766b11
kaitoseita <leadik2468@gmail.com>
2023-12-06 22:07:40 +0900

    WebViewのCookieを保持するためにDelegateを作成

--- QiitaArticleViewer/QiitaArticleViewerApp.swift
+++ QiitaArticleViewer/QiitaArticleViewerApp.swift
@@ -9,9 +9,50 @@ import SwiftUI
 
 @main
 struct QiitaArticleViewerApp: App {
+    
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             TopTabView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    private func storeCookies() {
+        guard let cookies = HTTPCookieStorage.shared.cookies else { return }
+        var cookieDictionary = [String : AnyObject]()
+        for cookie in cookies {
+            cookieDictionary[cookie.name] = cookie.properties as AnyObject?
+        }
+        UserDefaults.standard.set(cookieDictionary, forKey: "cookie")
+    }
+    
+    private func retrieveCookies() {
+        guard let cookieDictionary = UserDefaults.standard.dictionary(forKey: "cookie") else { return }
+        for (_, cookieProperties) in cookieDictionary {
+            if let cookieProperties = cookieProperties as? [HTTPCookiePropertyKey : Any] {
+                if let cookie = HTTPCookie(properties: cookieProperties ) {
+                    HTTPCookieStorage.shared.setCookie(cookie)
+                }
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        retrieveCookies()
+        return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        storeCookies()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        storeCookies()
     }
 }
